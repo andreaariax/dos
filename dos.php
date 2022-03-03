009cdc792e7f6551a6f4d5c771b86820ca6c5c9f
andreaariax <59872857+andreaariax@users.noreply.github.com>
2022-03-03 09:53:31 +0100

    Add files via upload

---
+++ dos.php
@@ -0,0 +1,31 @@
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Numeros Primos</title>
+</head><!DOCTYPE html>
+<body>
+	<form method="post" action="dos.php" name="formEsPrimo">
+		<input type="text" name="inputNumPrimo" required>
+		<br>
+		<button type="submit">Ver si es primo</button>
+	</form>
+</body>
+</html>
+<?php
+$numero = $_POST['inputNumPrimo'];
+$contadorPrimo = 0;
+
+for($contador = 1; $contador<=$numero; $contador++){
+	if($numero % $contador == 0){
+		$contadorPrimo = $contadorPrimo + 1;
+	}
+}
+
+if($numero == 1 or $contadorPrimo == 2){
+	echo "El numero ".$numero." es un numero primo";
+}
+else{
+	echo "El numero ".$numero." NO es un numero primo";
+}
+?>
